d4772a283bc5e4f20f151064728936e5d07c9397
jjinyeok <fun2314@naver.com>
2022-07-08 16:58:18 +0900

    [FEAT]#4 room API 맞춰 기본 세팅(Room, UserRoom 모델 생성)

---
+++ room/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from ..user.models import User
+
+# Create your models here.
+class Room(models.Model):
+    title = models.CharField(max_length=30)
+    target_time = models.DateTimeField()
+    start_time = models.DateTimeField(auto_now_add=True)
+    plan_half_fee = models.IntegerField()
+    plan_no_fee = models.IntegerField()
+    max_user_num = models.IntegerField()
+    room_code = models.CharField()
+    plan_period = models.IntegerField()
+    leader_id = models.IntegerField()
+    negative_percent = models.FloatField()
+    user_num = models.IntegerField()
+
+
+class UserRoom(models.Model):
+    user = models.ForeignKey(User, related_name='user_id', on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, related_name='room_id', on_delete=models.CASCADE)
--- user/serializers.py
+++ user/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('user', 'nickname', 'email')
+        fields = ('user', 'username', 'email')
