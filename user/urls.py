a5f4243215897c23b362c5dd6c4ba6dc50b99436
gahyun02 <102684258+gahyun02@users.noreply.github.com>
2022-08-20 04:39:40 +0900

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from .views import signin, signup, get_patch_delete, duplicationcheck
 urlpatterns = [
     path('signin/', signin), # 로그인
     path('signup/', signup), # 회원가입
-    path('', get_patch_delete), # 사용자 이름변경, 사용자 탈퇴
+    path('/', get_patch_delete), # 사용자 이름변경, 사용자 탈퇴
     path('duplicationcheck/', duplicationcheck), # 아이디 중복체크
     # path('check/', check), # 테스트용
 ]
