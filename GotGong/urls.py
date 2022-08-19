1a9fdce60b220b89b07b877e64dee6385f4030b5
gahyun02 <102684258+gahyun02@users.noreply.github.com>
2022-08-20 04:41:32 +0900

    Update urls.py

--- GotGong/urls.py
+++ GotGong/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user.urls')),
+    path('user/', include('user.urls')),
     path('room/', include('room.urls')),
     path('plan/', include('plan.urls')),
 ]
